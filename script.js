8a4aa4f40bcdf6eabe16361e0babeac08ba5c0ae
hafsah <emekomahafsah@gmail.com>
2020-03-26 18:20:09 +0100

    start.ng modification

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let name = "Hafsah Emekoma";
+let myCourses = ["HTML", " CSS", " JavaScript.", ];
+
+console.log("My name is " + name);
+console.log("My courses are "+ myCourses);
+  
+  if (myCourses.length % 2 !== 0){
+      
+    for(let i = 0; i <=200; i++){
+      if (i % 2 !==0){
+           console.log(i);
+       }
+   }
+}
